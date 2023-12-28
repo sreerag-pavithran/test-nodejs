b6a6de55e0d5a5a60b483fd1fec1be9531f5ed11
Sreerag Pavithran <sreeragpavithran@gmail.com>
2023-12-28 19:22:54 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,56 @@
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+
+var app = express();
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+app.get("/getDistanceMatrix", async (req, res) => {
+  const googleMapsUrl = `https://maps.googleapis.com/maps/api/distancematrix/json?${req.url.slice(
+    req.url.indexOf("?") + 1
+  )}`;
+
+  try {
+    const response = await fetch(googleMapsUrl);
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    console.error("Error fetching from Google Maps API:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+module.exports = app;
